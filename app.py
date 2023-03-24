1abe97ce21a80062d4a2d21a7487d2b3dce32b43
oghusky <philiposimmons@gmail.com>
2023-03-23 20:45:26 -0400

    changed db name

--- app.py
+++ app.py
@@ -2,29 +2,31 @@ from pymongo import MongoClient
 import os
 import sys
 import subprocess
-from flask import Flask,jsonify, render_template
+from flask import Flask, jsonify, render_template
 
 try:
-  import pandas as pd
-  from flask_cors import CORS
-  import sqlalchemy
-  from sqlalchemy.ext.automap import automap_base
-  from sqlalchemy.orm import Session
-  from sqlalchemy import create_engine
-  import psycopg2
+    import pandas as pd
+    from flask_cors import CORS
+    import sqlalchemy
+    from sqlalchemy.ext.automap import automap_base
+    from sqlalchemy.orm import Session
+    from sqlalchemy import create_engine
+    import psycopg2
 except ImportError:
-  subprocess.check_call([sys.executable, '-m', 'pip','install', 'pandas'])
-  subprocess.check_call([sys.executable, '-m', 'pip','install', 'flask_cors'])
-  subprocess.check_call([sys.executable, '-m', 'pip','install', 'sqlalchemy'])
-  subprocess.check_call([sys.executable, '-m', 'pip','install', 'psycopg2'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
+    subprocess.check_call(
+        [sys.executable, '-m', 'pip', 'install', 'flask_cors'])
+    subprocess.check_call(
+        [sys.executable, '-m', 'pip', 'install', 'sqlalchemy'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'psycopg2'])
 finally:
-  import pandas as pd
-  from flask_cors import CORS
-  import sqlalchemy
-  from sqlalchemy.ext.automap import automap_base
-  from sqlalchemy.orm import Session
-  from sqlalchemy import create_engine
-  import psycopg2
+    import pandas as pd
+    from flask_cors import CORS
+    import sqlalchemy
+    from sqlalchemy.ext.automap import automap_base
+    from sqlalchemy.orm import Session
+    from sqlalchemy import create_engine
+    import psycopg2
 
 pokedex = pd.read_csv('./resources/pokemon.csv').dropna()
 
@@ -38,12 +40,13 @@ CORS(app)
 client = MongoClient(os.getenv('MONGO_URI', 'mongodb://localhost:27017'))
 pokemongo = client.pokedex
 
-clean_pokemon = pd.read_csv('./resources/cleaned_pokemon.csv').to_dict('records')
+clean_pokemon = pd.read_csv(
+    './resources/cleaned_pokemon.csv').to_dict('records')
 
 # sql stuff
 engine = create_engine('postgresql://postgres:password@localhost/pokedex')
 Base = automap_base()
-Base.prepare(engine,reflect=True)
+Base.prepare(engine, reflect=True)
 
 # pd.read_csv('./resources/cleaned_pokemon.csv').to_sql('pokedex_two',con=engine)
 
@@ -64,45 +67,52 @@ Base.prepare(engine,reflect=True)
 #     'is_legend': item['Legendary'],
 #   })
 
-@app.route('/',methods=['GET']) #post, put(grab user by username and update the username), delete(grab by ID and delete)
+
+# post, put(grab user by username and update the username), delete(grab by ID and delete)
+@app.route('/', methods=['GET'])
 def home_page():
-  return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/two',methods=['GET']) #post, put(grab user by username and update the username), delete(grab by ID and delete)
+
+# post, put(grab user by username and update the username), delete(grab by ID and delete)
+@app.route('/two', methods=['GET'])
 def page_two():
-  return render_template('page_two.html')
+    return render_template('page_two.html')
 
-@app.route('/api/mongo_data',methods=['GET'])
+
+@app.route('/api/mongo_data', methods=['GET'])
 def mongo_data():
-  print(len(get_mongo_pokedata()))
-  return jsonify(data=get_mongo_pokedata())
+    print(len(get_mongo_pokedata()))
+    return jsonify(data=get_mongo_pokedata())
 
-@app.route('/api/sql_data',methods=['GET'])
+
+@app.route('/api/sql_data', methods=['GET'])
 def sql_data():
-  df = pd.read_sql('''SELECT * FROM pokedex_two''', con=engine)
-  sql_pokedata = df.to_dict('records')
-  return jsonify(sql_pokedata)
+    df = pd.read_sql('''SELECT * FROM pokedex''', con=engine)
+    sql_pokedata = df.to_dict('records')
+    return jsonify(sql_pokedata)
 
 
 def get_mongo_pokedata():
-  pokedata=[]
-  for item in list(pokemongo.pokedex.find()):
-    pokedata.append({
-      'index': item['index'],
-      'name': item['name'],
-      'type_one': item['type_one'],
-      'type_two': item['type_two'],
-      'total': item['total'],
-      'hp': item['hp'],
-      'attack': item['attack'],
-      'defense': item['defense'],
-      'sp_attack': item['sp_attack'],
-      'sp_defense': item['sp_defense'],
-      'speed': item['speed'],
-      'generation': item['generation'],
-      'is_legend': item['is_legend'],
-    })
-  return pokedata
+    pokedata = []
+    for item in list(pokemongo.pokedex.find()):
+        pokedata.append({
+            'index': item['index'],
+            'name': item['name'],
+            'type_one': item['type_one'],
+            'type_two': item['type_two'],
+            'total': item['total'],
+            'hp': item['hp'],
+            'attack': item['attack'],
+            'defense': item['defense'],
+            'sp_attack': item['sp_attack'],
+            'sp_defense': item['sp_defense'],
+            'speed': item['speed'],
+            'generation': item['generation'],
+            'is_legend': item['is_legend'],
+        })
+    return pokedata
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
